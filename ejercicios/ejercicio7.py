cdd7a94305adeee920dea55d0b45f0309ecc0f69
David Velez <velezdavidpino22@gmail.com>
2023-02-14 13:20:39 -0500

    add ejercicio2y3 refactor and renaname and ejercio7y8 functional

--- ejercicios/ejercicio7.py
+++
@@ -1,5 +0,0 @@
-class CuentaDeBanco:
-    def __init__(self, num_cuenta, propietarios, balance):
-        self.num_cuenta = num_cuenta
-        self.propietarios = propietarios
-        self.balance = balance
