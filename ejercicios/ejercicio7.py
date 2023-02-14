1c1b3a3f79beb6b3a1ebfb6cf53ba29d9c797f3e
David Velez <velezdavidpino22@gmail.com>
2023-02-14 13:12:20 -0500

    add ejercicio7 functional

---
+++ ejercicios/ejercicio7.py
@@ -0,0 +1,5 @@
+class CuentaDeBanco:
+    def __init__(self, num_cuenta, propietarios, balance):
+        self.num_cuenta = num_cuenta
+        self.propietarios = propietarios
+        self.balance = balance
